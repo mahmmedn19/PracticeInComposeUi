1076397fb2950580034bd98dc0c62e5f18f70092
Mohamed Naser <mahmmedn19@gmail.com>
2023-06-30 18:37:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.firstcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.toUpperCase
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.firstcompose.ui.theme.FirstComposeTheme
+import java.util.Locale
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting()
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Text(
+"Mohamed Naser",
+        color = Color.Gray,
+        fontFamily = FontFamily.SansSerif,
+        modifier = Modifier.background(Color.Black)
+    )
+}
+
+@Preview
+@Composable
+fun PreviewMessage(){
+    Greeting()
+}

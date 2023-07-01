4abfaaccb1ef52f9c192e2d795d587640227d3bf
Mohamed Naser <mahmmedn19@gmail.com>
2023-07-01 06:33:25 +0300

    practice in padding and margin

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -1,46 +1,70 @@
 package com.example.firstcompose
 
-import android.content.res.Configuration
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.firstcompose.ui.theme.FirstComposeTheme
-import java.util.Locale
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting()
+            MainContent()
         }
     }
 }
 
 @Composable
 fun Greeting() {
-    Text(
-"Mohamed Naser",
-        color = Color.Gray,
-        fontFamily = FontFamily.SansSerif,
-        modifier = Modifier.background(Color.Transparent)
-    )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            "Mohamed Naser",
+            color = Color.Gray,
+            fontFamily = FontFamily.SansSerif,
+            fontSize = 32.sp,
+            modifier = Modifier.background(Color.Transparent)
+                .fillMaxSize()
+                .border(2.dp, Color.Black) // outer border
+                .background(Color.Cyan)
+                .padding(8.dp) // space between the borders
+                .border(2.dp, Color.Green) // inner border
+                .padding(8.dp) // padding
+                .background(Color.Blue)
+        )
+        Text(
+            "Mohamed Naser",
+            color = Color.Gray,
+            fontFamily = FontFamily.SansSerif,
+            fontSize = 32.sp,
+            modifier = Modifier.background(Color.Transparent)
+        )
+
+    }
+
 }
 
-//@Preview(showSystemUi = true)
-@Preview(name = "arabic",locale = "ar")
-@Preview(name = "english",locale = "en")
+@Preview(showSystemUi = true)
 @Composable
 fun PreviewMessage(){
     Greeting()
